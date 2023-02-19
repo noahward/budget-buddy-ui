84517c81ad654920d8fa2adae74c702c9b073eed
Noah Ward <noah@Noahs-MacBook-Pro.local>
2023-02-19 14:47:17 -0700

    Create auth store

---
+++ src/stores/auth-store.ts
@@ -0,0 +1,34 @@
+import { defineStore } from 'pinia'
+// TODO: How to import router in store
+// import { router } from 'router' 
+import { api } from 'boot/axios'
+
+export const useAuthStore = defineStore('auth', {
+  state: () => ({
+    // TODO: Check if the below line is bad practice 
+    user: JSON.parse(localStorage.getItem('user') as string),
+    redirectUrl: undefined
+  }),
+  actions: {
+    async login (email: string, password:string) {
+      try {
+        const user = await api.post('/login', {email, password})
+        this.user = user
+        localStorage.setItem('user', JSON.stringify(user))
+        // router.push(this.redirectUrl || '/')
+      } catch (error) {
+        console.error(error)
+        // router.push('/login')
+      }
+    },
+    async logout () {
+      try {
+        await api.post('/logout', {token: this.user.token})
+      } catch (error) {
+        console.error(error)
+      }
+      this.user = null
+      // router.push('/login')
+    }
+  }
+})
