fb3b0b58af373474784d321f889bae16fb3b49c6
Noah Ward <noah@Noahs-MBP.localdomain>
2023-02-20 22:11:19 -0700

    Created login page

---
+++ src/pages/LoginPage.vue
@@ -0,0 +1,109 @@
+<template>
+  <q-page class="row justify-center content-center">
+    <q-card class="bg-grey-1 container">
+      <q-card-section class="q-mt-sm">
+        <div class="text-h6">
+          Welcome Back
+        </div>
+        <div class="text-subtitle2 text-grey-2">
+          Sign in to view your finances
+        </div>
+      </q-card-section>
+      <q-card-section>
+        <Form
+          v-slot="{ errors }"
+          :validation-schema="userSchema"
+          @submit="onSubmit"
+        >
+          <Field
+            v-slot="{ value, field }"
+            name="email"
+          >
+            <q-input
+              outlined
+              dense
+              label="Email"
+              type="email"
+              :model-value="value"
+              v-bind="field"
+              :class="{ 'q-mb-md': !errors.email }"
+            />
+          </Field>
+          <div
+            v-if="errors.email"
+            class="text-red text-caption row justify-start q-mb-sm"
+          >
+            <q-icon
+              name="error"
+              class="q-mr-xs self-center"
+            />
+            <span class="self-center">{{ errors.email }}</span>
+          </div>
+          <Field
+            v-slot="{ value, field }"
+            name="password"
+          >
+            <q-input
+              outlined
+              dense
+              label="Password"
+              type="password"
+              :model-value="value"
+              v-bind="field"
+            />
+          </Field>
+          <div
+            v-if="errors.password"
+            class="text-red text-caption row justify-start"
+          >
+            <q-icon
+              name="error"
+              class="q-mr-xs self-center"
+            />
+            <span class="self-center">{{ errors.password }}</span>
+          </div>
+          <q-btn
+            flat
+            no-caps
+            type="submit"
+            class="bg-primary text-white full-width q-mb-md q-mt-lg"
+            label="Sign In"
+          />
+        </Form>
+      </q-card-section>
+    </q-card>
+  </q-page>
+</template>
+
+<script setup lang="ts">
+import { object, string } from 'yup'
+import { Form, Field, configure } from 'vee-validate'
+import { useAuthStore } from 'stores/auth-store'
+
+const authStore = useAuthStore()
+
+const userSchema = object({
+  email: string().required('Email is required').email('Enter a valid email address'),
+  password: string().required('Password is required').min(6)
+})
+
+function onSubmit (values: any, actions: any) {
+  authStore.login(values)
+    .finally(() => {
+      actions.resetForm()
+    })
+}
+
+configure({
+  validateOnChange: false,
+  validateOnModelUpdate: false
+})
+</script>
+
+<style lang="scss">
+.container {
+  max-width: 350px;
+  width: 100%;
+  text-align: center;
+}
+</style>
