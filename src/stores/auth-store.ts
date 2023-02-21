802e4e61996722b62ea373921761862c3802b7f8
Noah Ward <noah@Noahs-MBP.localdomain>
2023-02-20 22:12:12 -0700

    Update auth store login method

--- src/stores/auth-store.ts
+++ src/stores/auth-store.ts
@@ -1,18 +1,18 @@
 import { defineStore } from 'pinia'
 // TODO: How to import router in store
-// import { router } from 'router' 
+// import { router } from 'router'
 import { api } from 'boot/axios'
 
 export const useAuthStore = defineStore('auth', {
   state: () => ({
-    // TODO: Check if the below line is bad practice 
+    // TODO: Check if the below line is bad practice
     user: JSON.parse(localStorage.getItem('user') as string),
     redirectUrl: undefined
   }),
   actions: {
-    async login (email: string, password:string) {
+    async login (userInfo: object) {
       try {
-        const user = await api.post('/login', {email, password})
+        const user = await api.post('/login', { userInfo })
         this.user = user
         localStorage.setItem('user', JSON.stringify(user))
         // router.push(this.redirectUrl || '/')
@@ -23,7 +23,7 @@ export const useAuthStore = defineStore('auth', {
     },
     async logout () {
       try {
-        await api.post('/logout', {token: this.user.token})
+        await api.post('/logout', { token: this.user.token })
       } catch (error) {
         console.error(error)
       }
