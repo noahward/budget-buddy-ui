259f3938d66350d4e682f9149d49f9a79731d022
Noah Ward <noahward@shaw.ca>
2023-02-22 16:31:09 -0700

    Properly configure auth headers in boot

--- src/boot/axios.ts
+++ src/boot/axios.ts
@@ -8,28 +8,21 @@ declare module '@vue/runtime-core' {
   }
 }
 
-// Be careful when using SSR for cross-request state pollution
-// due to creating a Singleton instance here;
-// If any client changes this (global) instance, it might be a
-// good idea to move this instance creation inside of the
-// "export default () => {}" function below (which runs individually
-// for each client)
-interface APIConfig {
-  baseURL: string | undefined;
-  Authorization?: string
-}
+const api = axios.create({ baseURL: import.meta.env.VITE_API_BASE_URL })
 
-const config: APIConfig = { baseURL: import.meta.env.VITE_API_BASE_URL }
-const { user } = useAuthStore()
+export default boot(({ app, store }) => {
+  const { user } = useAuthStore(store)
+  if (user.data) {
+    api.defaults.headers.common['Authorization'] = `Token ${user.data.token}`
+  }
 
-if (user?.token) {
-  config.Authorization = `Token ${user.token}`
-}
-
-const api = axios.create(config)
-
-export default boot(({ app }) => {
-  // for use inside Vue files (Options API) through this.$axios and this.$api
+  // TODO: Are boolean environment variables possible?
+  if (import.meta.env.VITE_LOG_REQUESTS === 'true') {
+    api.interceptors.request.use(request => {
+      console.log('Starting request:', request)
+      return request
+    })
+  }
 
   app.config.globalProperties.$axios = axios
   // ^ ^ ^ this will allow you to use this.$axios (for Vue Options API form)
